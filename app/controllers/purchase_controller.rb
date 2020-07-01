734dcd4c373a8ddf5a6a477cb4798beb75eaf834
teruyukichikahisa <teruyuki19851220@yahoo.co.jp>
2020-07-01 13:09:00 +0900

    fix

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -4,15 +4,13 @@ class PurchaseController < ApplicationController
   def index
     @items = Item.includes(:images).order('created_at DESC')
     card = CreditCard.find_by(user_id: current_user.id)
-    索
     if card.blank?
       
-      redirect_to controller: "card", action: "new"
+      redirect_to controller: "credit_cards", action: "new"
     else
       Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
       
       customer = Payjp::Customer.retrieve(card.customer_id)
-      めインスタンス変数に代入
       @default_card_information = customer.cards.retrieve(card.card_id)
       @items = Item.includes(:images).order('created_at DESC')
     end
