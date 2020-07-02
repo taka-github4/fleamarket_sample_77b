438cacedf8aa52eb7554c8692216a95930617bb2
taka-github4 <65590087+taka-github4@users.noreply.github.com>
2020-07-02 14:27:33 +0900

    fix pay jp

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,34 +1,27 @@
-document.addEventListener(
-  "DOMContentLoaded", e => {
-    if (document.getElementById("token_submit") != null) {
-      Payjp.setPublicKey("pk_test_e58090c01c48530e024bc527");
-      let btn = document.getElementById("token_submit");
-      btn.addEventListener("click", e => {
-        e.preventDefault();
-        let card = {
-          number: document.getElementById("card_number").value,
-          cvc: document.getElementById("cvc").value,
-          exp_month: document.getElementById("exp_month").value,
-          exp_year: document.getElementById("exp_year").value
-        };
-        Payjp.createToken(card, (status, response) => {
-          if (status === 200) {
-            $("#card_number").removeAttr("name");
-            $("#cvc").removeAttr("name");
-            $("#exp_month").removeAttr("name");
-            $("#exp_year").removeAttr("name");
-            $("#card_token").append(
-              $('<input type="hidden" name="payjp-token">').val(response.id)
-            );
-            
-            alert("登録が完了しました");
-          } else {
-            alert("カード情報が正しくありません。");
-          }
-          document.inputForm.submit();
-        });
-      });
-    }
-  },
-  false
-);
+$(document).on('turbolinks:load', ()=> {
+  $('#token_submit').on('click', function (e) {
+    e.preventDefault();
+    Payjp.setPublicKey("pk_test_e58090c01c48530e024bc527");
+    let card = {
+      number: document.getElementById("card_number").value,
+      cvc: document.getElementById("cvc").value,
+      exp_month: document.getElementById("exp_month").value,
+      exp_year: document.getElementById("exp_year").value
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status === 200) {
+        $("#card_token").append(
+          $('<input type="hidden" name="payjp-token">').val(response.id)
+        );
+        $("#card_number").val("")
+        $("#cvc").val("")
+        $("#exp_month").val("")
+        $("#exp_year").val("")
+        alert("登録が完了しました");
+      } else {
+        alert("カード情報が正しくありません。");
+      }
+      document.inputForm.submit();
+    });
+  });
+})
