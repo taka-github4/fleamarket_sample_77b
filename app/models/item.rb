67eefaf81cf6009b6f883e957645daa02dd44f26
eika-yamashita <skdc0106@gmail.com>
2020-07-06 18:45:45 +0900

    fix favorite

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :category
   has_many :photos, dependent: :destroy
   validates :photos, presence: true, allow_nil: true
